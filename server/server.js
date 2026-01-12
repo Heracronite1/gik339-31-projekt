4c86e663c4d866d47890878e51b0516d274614ff
Heracronite1 <filip@init5.net>
2026-01-12 17:16:49 +0100

    Implement videogame CRUD backend using Express and SQLite
    + Replaced user-based endpoints with videogame resource
    
    + Updated routes to /games (GET, POST, PUT, DELETE)
    
    + Aligned SQLite queries with games table schema
    
    + Switched to parameterized SQL statements
    
    + Returned JSON responses for frontend feedback

--- server/server.js
+++ server/server.js
@@ -1,119 +1,103 @@
-/* Importera npm-paket sqlite3 med hjälp av require() och lagrar i variabeln sqlite */
 const sqlite = require('sqlite3').verbose();
-/* Skapar ny koppling till databas-fil som skapades tidigare. */
 const db = new sqlite.Database('./gik339.db');
 
-/* Importerar npm-paket express och lagrar i variabeln express */
 const express = require('express');
-/* Skapar server med hjälp av express */
 const server = express();
 
-/* Sätter konfiguration på servern */
+// ===== MIDDLEWARE =====
 server
-  /* Data ska kommuniceras i JSON-format */
   .use(express.json())
-  /* Sättet som data ska kodas och avkodas på */
   .use(express.urlencoded({ extended: false }))
   .use((req, res, next) => {
-    /* Headers för alla förfrågningar. Hanterar regler för CORS (vilka klienter som får anropa vår server och hur.) */
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', '*');
     res.header('Access-Control-Allow-Methods', '*');
-    /* Säger åt servern att fortsätta processa förfrågan */
     next();
   });
 
-/* Startar servern på port 3000 */
+// ===== START SERVER =====
 server.listen(3000, () => {
-  /* Meddelande för feedback att servern körs */
   console.log('Server running on http://localhost:3000');
 });
 
-/* Hantering av GET-requests till endpointen /users */
-server.get('/users', (req, res) => {
-  /* sql-query för att hämta alla users ur databasen. */
-  const sql = 'SELECT * FROM users';
-  /* Anrop till db-objektets funktion .all som används till att hämta upp rader ur en tabell */
+// ===== GET ALL GAMES =====
+server.get('/games', (req, res) => {
+  const sql = 'SELECT * FROM games';
+
   db.all(sql, (err, rows) => {
-    /* Callbackfunktionen har parametern err för att lagra eventuella fel */
     if (err) {
-      /* Om det finns något i det objektet skickar vi ett svar tillbaka att något gick fel (status 500) och info om vad som gick fel (innehållet i objektet err) */
-      res.status(500).send(err);
+      res.status(500).send({ error: err.message });
     } else {
-      /* Annars, om allt gick bra, skickar vi de rader som hämtades upp.  */
       res.send(rows);
     }
   });
 });
 
-server.get('/users/:id', (req, res) => {
+// ===== GET ONE GAME =====
+server.get('/games/:id', (req, res) => {
   const id = req.params.id;
+  const sql = 'SELECT * FROM games WHERE id = ?';
 
-  const sql = `SELECT * FROM users WHERE id=${id}`;
-
-  db.all(sql, (err, rows) => {
+  db.get(sql, [id], (err, row) => {
     if (err) {
-      res.status(500).send(err);
+      res.status(500).send({ error: err.message });
     } else {
-      res.send(rows[0]);
+      res.send(row);
     }
   });
 });
 
-server.post('/users', (req, res) => {
-  const user = req.body;
-  const sql = `INSERT INTO users(firstName, lastName, username, color) VALUES (?,?,?,?)`;
+// ===== CREATE GAME =====
+server.post('/games', (req, res) => {
+  const { title, studio, genre, platform } = req.body;
 
-  db.run(sql, Object.values(user), (err) => {
+  const sql =
+    'INSERT INTO games (title, studio, genre, platform) VALUES (?, ?, ?, ?)';
+
+  db.run(sql, [title, studio, genre, platform], function (err) {
     if (err) {
-      console.log(err);
-      res.status(500).send(err);
+      res.status(500).send({ error: err.message });
     } else {
-      res.send('Användaren sparades');
+      res.send({
+        message: 'Game added',
+        id: this.lastID
+      });
     }
   });
 });
 
-server.put('/users', (req, res) => {
-  const bodyData = req.body;
+// ===== UPDATE GAME =====
+server.put('/games', (req, res) => {
+  const { id, title, studio, genre, platform } = req.body;
 
-  const id = bodyData.id;
-  const user = {
-    firstName: bodyData.firstName,
-    lastName: bodyData.lastName,
-    username: bodyData.username,
-    color: bodyData.color
-  };
+  const sql =
+    'UPDATE games SET title = ?, studio = ?, genre = ?, platform = ? WHERE id = ?';
 
-  let updateString = '';
-  const columnsArray = Object.keys(user);
-  columnsArray.forEach((column, i) => {
-    updateString += `${column}="${user[column]}"`;
-    if (i !== columnsArray.length - 1) updateString += ',';
-  });
-  const sql = `UPDATE users SET ${updateString} WHERE id=${id}`;
-
-  db.run(sql, (err) => {
+  db.run(sql, [title, studio, genre, platform, id], function (err) {
     if (err) {
-      console.log(err);
-      res.status(500).send(err);
+      res.status(500).send({ error: err.message });
     } else {
-      res.send('Användaren uppdaterades');
+      res.send({
+        message: 'Game updated',
+        changes: this.changes
+      });
     }
   });
-  //UPDATE users SET firstName="Mikaela",lastName="Hedberg" WHERE id=1
 });
 
-server.delete('/users/:id', (req, res) => {
+// ===== DELETE GAME =====
+server.delete('/games/:id', (req, res) => {
   const id = req.params.id;
-  const sql = `DELETE FROM users WHERE id = ${id}`;
+  const sql = 'DELETE FROM games WHERE id = ?';
 
-  db.run(sql, (err) => {
+  db.run(sql, [id], function (err) {
     if (err) {
-      console.log(err);
-      res.status(500).send(err);
+      res.status(500).send({ error: err.message });
     } else {
-      res.send('Användaren borttagen');
+      res.send({
+        message: 'Game deleted',
+        changes: this.changes
+      });
     }
   });
 });
